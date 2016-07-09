fbd2bde650408708d4ebab5268306d57d662f030
czhtest <371724828@qq.com>
2016-07-09 10:00:20 +0800

    code style impove

--- www/test.py
+++ www/test.py
@@ -47,44 +47,109 @@ class MyHTMLParser(HTMLParser):
     def handle_entityref(self, name):
         print('&#%s ' % name)
 
-import re
+'''
+改进版本不使用全局变量__is_meeting前面__代表该变量是私有变量
+'''
 
 class MyHTMLParser1(HTMLParser):
-
+    __is_meeting = False
+    #self表示创建的实例本身
     def handle_starttag(self, tag, attrs):
         #print('%s %s',tag,attrs)
-        a = re.compile(r'/events/python-events/\d{3}')
+
         if tag=='a' and len(attrs) > 0:
             for k,v in attrs:
                 if k=='href' and '/events/python-events/' in v:
                     if v[-4:-1].isdigit():
-                        gobal.IsMeeting = True
+                        self.__is_meeting = True
         elif tag=='time' and len(attrs) >0:
             for k,v in attrs:
                 if k =='datetime':
                     #print(v)
-                    gobal.IsMeeting = True
+                    self.__is_meeting = True
         elif tag=='span' and len(attrs) > 0:
             for k,v in attrs:
                 if k == 'class' and v=='event-location':
-                    gobal.IsMeeting = True
+                    self.__is_meeting = True
                 else:
-                    gobal.IsMeeting = False
+                    self.__is_meeting = False
                     #print('is meeting',ISMEETING)
         else:
-            gobal.IsMeeting = False
+            self.__is_meeting = False
     def handle_charref(self, name):
         print(name)
     def handle_data(self, data):
 
         #print('is meeting',ISMEETING)
-        if gobal.IsMeeting == True:
+        if self.__is_meeting == True:
             print(data)
     def handle_entityref(self, name):
         print('&#%s ' % name)
+'''
+生成dict{'meeting name':'',...}
+'''
+class MyHTMLParser2(HTMLParser):
+    is_meeting = False
+    __Meetings =[]
+    #self表示创建的实例本身
+    def handle_starttag(self, tag, attrs):
+        #print('%s %s',tag,attrs)
+
+        if tag=='a' and len(attrs) > 0:
+            for k,v in attrs:
+                if k=='href' and '/events/python-events/' in v:
+                    if v[-4:-1].isdigit():
+                        self.is_meeting = True
+                    else:
+                        self.is_meeting = False
+                else:
+                       self.is_meeting = False
+        elif tag=='time' and len(attrs) >0:
+            for k,v in attrs:
+                if k =='datetime':
+                    #print(v)
+                    self.is_meeting = True
+                else:
+                    self.is_meeting = False
+        elif tag=='span' and len(attrs) > 0:
+            for k,v in attrs:
+                if k == 'class' and v=='event-location':
+                    self.is_meeting = True
+                else:
+                    self.is_meeting = False
 
 
+        else:
+            self.is_meeting = False
+    def handle_charref(self, name):
+        print(name)
+    def handle_data(self, data):
 
-parser = MyHTMLParser()
-for line in open('python-events.txt','r'):
+        #print('is meeting',ISMEETING)
+        if self.is_meeting == True:
+            self.__Meetings.append(data)
+            #print(data)
+    def handle_entityref(self, name):
+        print('&#%s ' % name)
+    def Show(self):
+
+        for k in self.__Meetings:
+
+            print( k)
+
+        #print('self meetings %s...' % self.__Meetings)
+import urllib.request,io,os,sys
+url='https://www.python.org/events/python-events/'
+req = urllib.request.Request(url)
+f = urllib.request.urlopen(req)
+s = f.read()
+s = s.decode('utf-8')
+with open('admint.txt','w',100,'utf-8') as file:
+    file.write(s)
+
+
+parser = MyHTMLParser2()
+for line in open('admint.txt','r'):
     parser.feed(line.strip())
+parser.Show()
+parser.close()
