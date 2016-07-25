f768a97673daf8c10fb9bd7ab7596f80437da8c5
czhtest <371724828@qq.com>
2016-07-25 14:25:34 +0800

    添加每日除了爬取微博，增加获取统计计算的调用，并发送表格

--- www/TuShare.py
+++ www/TuShare.py
@@ -2,6 +2,8 @@ import tushare as ts
 import math
 import pandas as pd
 import numpy as np
+import dateFrameTemplate as dft
+import OwnTime as ot
 
 #历史行情
 #code：股票代码，即6位数字代码，或者指数代码
@@ -9,8 +11,7 @@ import numpy as np
 import time
 from datetime import timedelta,date
 import datetime
-def GetNowDate():
-    return time.strftime('%Y-%m-%d',time.localtime(time.time()))
+
 #df = ts.get_hist_data('sh',start='2016-07-20',ktype='5')
 #print(df['close'])
 '''
@@ -21,37 +22,41 @@ ktype：数据类型，D=日k线 W=周 M=月 5=5分钟 15=15分钟 30=30分钟 6
 
 
 #先建一个索引
-rowIndexs=[5,15,30,60,'D']
-cols=['MA14','MA60','MA99','MA888','LOW','UP']
-dayframe = pd.DataFrame(index=rowIndexs,columns=cols)
 
-'''
-#print(df)
-#设置值
-#print(list(df.index))
-values={
-    14:lambda index:MA(days=14,ktype=str(index)),
-    60:lambda index:MA(days=60,ktype=str(index)),
-    99:lambda index:MA(days=99,ktype=str(index)),
-    888:lambda index:MA(days=888,ktype=str(index))
-}
-df.MA14=list(map(values[14],df.index))
-df.MA60=list(map(values[60],df.index))
-df.MA99=list(map(values[99],df.index))
-df.MA888=list(map(values[888],df.index))
-#print(df)
-'''
 #定义获取某日的值,
 #注意:当ktype等于D时，开始可以等于结束,获取1天，总共获取end-start+1,包含了结束本身
 #当ktype等于5,15,30,60时，则不包含end当天数据
-def GetDateData(code='sh',end=GetNowDate(),ktype='D'):
+'''
+BOLL-M 算法
+N:=20;
+MID:=MA(C,N);
+VAR1:=POW((C-MID),2);
+VAR2:=MA(VAR1,N);
+VAR3:=SQRT(VAR2);
+UPPER:=MID+2*VAR3;
+LOWER:=MID-2*VAR3;
+BOLL:REF(MID,1);
+UB:REF(UPPER,1);
+LB:REF(LOWER,1);
+
+MID赋值:收盘价的N日简单移动平均
+VAR1赋值:(收盘价-MID)的2乘幂
+VAR2赋值:VAR1的N日简单移动平均
+VAR3赋值:VAR2的开方
+UPPER赋值:MID+2*VAR3
+LOWER赋值:MID-2*VAR3
+输出BOLL:1日前的MID
+输出UB:1日前的UPPER
+输出LB:1日前的LOWER
+'''
+def GetDateData(code='sh',end=ot.GetNowDate(),ktype='D'):
     #为了数据准确,先尝试获取5分钟数据,以第一条5分钟数据为准
     realEnd=end
-    endDate=datetime.datetime.strptime(realEnd,'%Y-%m-%d').date()
+    #print(end,ktype,'...')
     if ktype !='D':
         #需要结束日期自动后延一天
-        realEnd = (endDate+timedelta(days=1)).strftime('%Y-%m-%d')
-
+        realEnd = ot.GetOneMoreDate(end)
+    #print(realEnd,ktype)
     dh = ts.get_hist_data(code,end=realEnd,ktype=ktype)
     #将结果集进行升序排列,为了rolling获取值方便
     dh.sort_index(inplace=True,ascending=True)
@@ -61,6 +66,8 @@ def GetDateData(code='sh',end=GetNowDate(),ktype='D'):
         colName='MA'+str(ma)
         dh[colName]=round(dh['close'].rolling(window=ma).mean(),2)
     #第二步计算自定义的BOLL-M，得到的注意并未偏转一天
+	#此处跟一般的股票软件计算方式有区别，
+	
     colName='LOW'
     dh[colName]=round(dh['MA99']-2*((((dh['close']-dh['MA99']).apply(lambda x:x**2)).rolling(window=99).mean())**0.5),2)
     colName='UP'
@@ -68,24 +75,26 @@ def GetDateData(code='sh',end=GetNowDate(),ktype='D'):
     return dh[['close','MA14','MA60','MA99','MA888','LOW','UP']]
 
 
-#print(GetDateData(end='2016-07-23',ktype='5').tail(4))
+#print(GetDateData(end='2016-07-22',ktype='5').tail(4))
 
-#dh = ts.get_hist_data('sh',start='2016-07-20',end='2016-07-20',ktype='5')
+#dh = ts.get_hist_data('sh',start='2016-D07-20',end='2016-07-20',ktype='5')
 #print(dh)
 
 
 #获取值
-def GetMyOwnDate(date=GetNowDate()):
-    print(GetDateData(end=date).tail(1).index[0])
+def GetMyOwnDate(date=ot.GetNowDate()):
+    date=GetDateData(end=date).tail(1).index[0]
+    print(date)
 
-    for irow in range(len(dayframe.index)):
-        ktype=dayframe.index[irow]
+    for irow in range(len(dft.dayframe.index)):
+        ktype=dft.dayframe.index[irow]
         data = GetDateData(end=date,ktype=str(ktype)).tail(1)
         #print(ktype,data)
-        for icol in range(len(dayframe.columns)):
+        for icol in range(len(dft.dayframe.columns)):
             itemp=icol+1
-            dayframe.iat[irow,icol]=data.iat[0,itemp]
+            dft.dayframe.iat[irow,icol]=data.iat[0,itemp]
 
-    print(dayframe)
+    print(dft.dayframe)
+    dft.dayframe.to_excel('日统计.xls',sheet_name=date)
 
-GetMyOwnDate('2016-07-20')
+GetMyOwnDate()
